19f8d591698438f1b9a5ed398537f322bcebaa68
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2025-06-25 16:31:42 +0530

    feat: add main attendance monitoring loop with face verification

---
+++ face_attendance/main.py
@@ -0,0 +1,70 @@
+# main.py
+import cv2
+import sqlite3
+import numpy as np
+from utils.face_utils import get_face_embedding, cosine_similarity
+from utils.db_utils import create_db
+from datetime import datetime
+
+create_db()
+conn = sqlite3.connect("database/students.db")
+cursor = conn.cursor()
+
+cursor.execute("SELECT id, name, embedding FROM students")
+known = [
+    (row[0], row[1], np.frombuffer(row[2], dtype=np.float32))
+    for row in cursor.fetchall()
+]
+
+attendance_set = set()
+cap = cv2.VideoCapture("/home/icebyte/Projects/Personal/Python/Computer_Vision/face-recognition/face_attendance/test02.mp4")
+print("[INFO] Press 'q' to quit. Automatically marking attendance...")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frame = cv2.resize(frame, (1280, 720))
+
+    face, emb = get_face_embedding(frame)
+    if emb is not None:
+        matched = False
+        for sid, name, k_emb in known:
+            sim = cosine_similarity(emb, k_emb)
+            if sim > 0.6 and sid not in attendance_set:
+                attendance_set.add(sid)
+                cursor.execute(
+                    "INSERT INTO attendance (id, name) VALUES (?, ?)", (sid, name)
+                )
+                conn.commit()
+                print(
+                    f"[ATTENDANCE] {name} marked at {datetime.now().strftime('%H:%M:%S')}"
+                )
+                bbox = face.bbox.astype(int)
+                # Drawing simple blue box initially
+                cv2.rectangle(
+                    frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 1
+                )
+                cv2.putText(
+                    frame,
+                    f"{name}",
+                    (bbox[0], bbox[1] - 5),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (255, 0, 0),
+                    1,
+                )
+                matched = True
+                break
+        if not matched:
+            cv2.putText(
+                frame, "Unknown", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2
+            )
+
+    cv2.imshow("Attendance", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+conn.close()
